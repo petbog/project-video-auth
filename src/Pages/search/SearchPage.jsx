264650dcef53fc1e22436357268fd6c86b75fa29
Bogdan <bogdan_emdetei_petrov@mail.ru>
2023-04-05 13:53:49 +0300

    сделал popup

--- src/Pages/search/SearchPage.jsx
+++ src/Pages/search/SearchPage.jsx
@@ -2,7 +2,7 @@ import { useAuth } from '../../hooks/use-auth'
 import { useDispatch, useSelector } from 'react-redux';
 import { removeUser } from '../../Redux/slise/UserSlice'
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import classes from './SearchPage.module.css'
 import SerchVideo from '../../Component/search/SerchVideo';
 import { SearchVideo } from '../../Redux/slise/SearchSlise';
@@ -12,8 +12,10 @@ import grid_activ from '../../img/grid_activ.png'
 import grid_desibel from '../../img/grid_desibel.png'
 import list_activ from '../../img/list_activ.png'
 import list_desibel from '../../img/list_desibel.png'
+import setting from '../../img/setting.svg'
 import MyPreloader from '../../preloader/Preloader'
 import simons from '../../img/92513305_simons_cat_013450x450svg.png'
+import Modal from '../../Component/popup/Modal'
 
 
 
@@ -24,16 +26,32 @@ const SearchPage = () => {
     const video = item.items
     const navigate = useNavigate()
     const { isAuth, email } = useAuth()
-    const [statusGrid,setStatusGrid]=useState(grid_activ)
-    const [statusList,setStatusList]=useState(list_desibel)
-    const [statusGridTrue,setStatusGridTrue]=useState(true)
+    const [statusGrid, setStatusGrid] = useState(grid_activ)
+    const [statusList, setStatusList] = useState(list_desibel)
+    const [statusGridTrue, setStatusGridTrue] = useState(true)
+    const [settingMenu, SetSettingMenu] = useState(false)
+    const imgRef = useRef(null)
 
-    const handleStatusVideoList=()=>{
-        if(statusGrid === grid_activ){
+
+    useEffect(() => {
+        const handleClickOutsade = (event) => {
+            if (imgRef.current && !event.composedPath().includes(imgRef.current)) {
+                // SetsearchMenu(false)
+            }
+        }
+        document.body.addEventListener('click', handleClickOutsade)
+
+        return () => {
+            document.body.removeEventListener('click', handleClickOutsade)
+        }
+    }, [])
+
+    const handleStatusVideoList = () => {
+        if (statusGrid === grid_activ) {
             setStatusList(grid_desibel)
             setStatusGrid(list_activ)
             setStatusGridTrue(false)
-        }else{
+        } else {
             setStatusList(list_desibel)
             setStatusGrid(grid_activ)
             setStatusGridTrue(true)
@@ -54,7 +72,7 @@ const SearchPage = () => {
     }, [isAuth, navigate])
 
     const skeleton = [...new Array(8)].map((_, i) => <MyPreloader key={i} />);
-    
+
     return isAuth ? (
         <div className={classes.home}>
             <div className={classes.background_header}></div>
@@ -74,16 +92,20 @@ const SearchPage = () => {
                         <div className={classes.img_sort}>
                             <img onClick={handleStatusVideoList} className={classes.img_sort_list} src={statusGrid} alt="" />
                             <img onClick={handleStatusVideoList} className={classes.img_sort_list} src={statusList} alt="" />
+                            <img onClick={() => SetSettingMenu(!settingMenu)} ref={imgRef} className={classes.img_sort_setting} src={setting} alt="" />
+                            {
+                                settingMenu && <Modal/>
+                            }
                         </div>
                     </div>
                     <div className={`${statusGrid === grid_activ ? classes.grid : classes.flex}`}>
                         {
-                            video ?  video.map((item, index) =>
+                            video ? video.map((item, index) =>
                                 <Video item={item} key={index} statusGridTrue={statusGridTrue} />
-                            ):skeleton
+                            ) : skeleton
                         }
                     </div>
-                    
+
                 </div>
 
             </div>
