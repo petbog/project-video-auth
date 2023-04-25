c36cc0e92efac63b1661e70f4f2015d16a680772
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2023-04-25 21:31:14 +0300

    сделал валидацию форм

--- src/Component/FormAuth/Form.jsx
+++ src/Component/FormAuth/Form.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import {useState } from "react"
 import classes from './Form.module.css'
 import eye_off from '../../img/eye_off.svg'
 import eye from '../../img/eye.svg'
@@ -10,43 +10,88 @@ const Form = ({ title, handleClick }) => {
 
     const [email, setEmail] = useState('')
     const [pass, setPass] = useState('')
-    const [type,setType]=useState("password")
-    const [src,setSrc]=useState(eye_off)
+    const [type, setType] = useState("password")
+    const [src, setSrc] = useState(eye_off)
+    const [emailDirty, setEmailDirty] = useState(false)
+    const [passDirty, setPassDirty] = useState(false)
+    const [emailError, setEmailError] = useState('Gmail не может быть пустым')
+    const [passError, setPassError] = useState('Пароль не может быть пустым')
 
-
-const handleToggle=()=>{
-    if(type === "password"){
-        setType("text")
-        setSrc(eye)
-    }else{
-        setType("password")
-        setSrc(eye_off)
+    const handleToggle = () => {
+        if (type === "password") {
+            setType("text")
+            setSrc(eye)
+        } else {
+            setType("password")
+            setSrc(eye_off)
+        }
     }
-}
+
+    const blurHandle = (e) => {
+        switch (e.target.name) {
+            case 'email':
+                setEmailDirty(true)
+                break
+            case 'password':
+                setPassDirty(true)
+                break
+        }
+    }
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value)
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError('Некоректный gmail')
+        } else {
+            setEmailError('')
+        }
+
+    }
+
+    const passwordHandler = (e) => {
+        setPass(e.target.value)
+        if (e.target.value.length < 6 || e.target.value.length > 9) {
+            setPassError('Пароль должен состоять от 6 до 8 символов')
+            if (!e.target.value) {
+                setPassError('Пароль должен состоять от 6 до 8 символов')
+            }
+        } else {
+            setPassError('')
+        }
+    }
 
     return (
         <div className={classes.Form_inner}>
             <div className={classes.Form_container}>
                 <p className={classes.form_title}>Логин</p>
-                <input className={classes.Form_email} type='email'
+                <input
+                    name="email"
+                    onBlur={(e) => blurHandle(e)}
+                    className={classes.Form_email}
+                    type='email'
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={(e) => emailHandler(e)}
                     placeholder='gmail'
                 />
+                {(emailDirty && emailError) && <div className={classes.errorPoppup}>{emailError}</div>}
             </div>
             <div className={classes.Form_container}>
                 <p className={classes.form_title}>Пароль</p>
                 <input
+                    name="password"
+                    onBlur={(e) => blurHandle(e)}
                     className={classes.Form_pass}
                     type={type}
                     value={pass}
-                    onChange={(e) => setPass(e.target.value)}
+                    onChange={(e) => passwordHandler(e)}
                     placeholder='password'
                 />
+                {(passDirty && passError) && <div className={classes.errorPoppup}>{passError}</div>}
                 <img onClick={handleToggle} className={classes.form_img} src={src} alt="" />
             </div>
             <div className={classes.Button_container}>
-                <button className={classes.Form_button} onClick={() => handleClick(email, pass)}>{title}</button>
+                <button  className={classes.Form_button} onClick={() => handleClick(email, pass)}>{title}</button>
             </div>
         </div>
     )
